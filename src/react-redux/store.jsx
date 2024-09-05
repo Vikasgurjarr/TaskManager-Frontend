781c835ed90f60fe6010da5660c24dfeae1addac
Vikasgurjarr <gurjaravs@gmail.com>
2024-09-05 16:13:30 +0530

    change

--- src/react-redux/store.jsx
+++ src/react-redux/store.jsx
@@ -1,5 +1,5 @@
 import { createStore, applyMiddleware } from "redux";
-import  thunk  from "redux-thunk"; // Importing thunk correctly
+import { thunk } from "redux-thunk"; // Importing thunk correctly
 import { composeWithDevTools } from "redux-devtools-extension";
 import rootReducer from "./rootReducer"; // Replace with your actual root reducer path
 
